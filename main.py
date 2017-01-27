6f3212f60a1bc9c9daa493b95acd0d85c5fcd9d9
Piotr Luboń <p.lubon@omegaitc.pl>
2017-01-27 14:44:07 +0100

    Binary mode

--- main.py
+++ main.py
@@ -5,19 +5,29 @@ from DecisionTree import DecisionTree
 def main():
     if len(argv)>=4:
         if(argv[1] == '-t'):
-            training_data = DataReader.read_training_data_multi(argv[2])
+            training_data = None
+            if '-b' in argv:
+                training_data =  DataReader.read_training_data_binary(argv[2])
+            else:
+                training_data = DataReader.read_training_data_multi(argv[2])
             tree = DecisionTree()
             tree.train(training_data[0], training_data[1])
             tree.save(argv[3])
         elif(argv[1] == '-v'):
-            training_data = DataReader.read_training_data_multi(argv[2])
+            if '-b' in argv:
+                training_data =  DataReader.read_training_data_binary(argv[2])
+            else:
+                training_data = DataReader.read_training_data_multi(argv[2])
             tree = DecisionTree()
             tree.load(argv[3])
             results = tree.validate(training_data[0], training_data[1])
             with open(argv[4], 'wb') as outfile:
                 outfile.write("\n".join(results))
         elif(argv[1] == '-c'):
-            input_data = DataReader.read_input_data(argv[2])
+            if '-b' in argv:
+                training_data =  DataReader.read_training_data_binary(argv[2])
+            else:
+                training_data = DataReader.read_training_data_multi(argv[2])
             tree = DecisionTree()
             tree.load(argv[3])
             results = tree.classify(input_data)
